b273bf82b85c782ba7a3228cb468aded8040fa46
indramukti <96394744+indramukti@users.noreply.github.com>
2022-10-18 16:14:40 +0700

    add db

---
+++ src/main/java/toko/barang/BarangApplication.java
@@ -0,0 +1,13 @@
+package toko.barang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toko/barang/BarangApplicationTests.java
@@ -0,0 +1,13 @@
+package toko.barang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
